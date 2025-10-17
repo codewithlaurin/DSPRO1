2a908d0ac1d451774b4c6ae958c5faf1450a6af9
jaybrandon <jaybrandon@outlook.com>
2025-10-17 17:10:49 +0200

    feat: change data splitting for cross validation

--- src/dataset.py
+++ src/dataset.py
@@ -3,7 +3,6 @@ import shutil
 
 import kagglehub
 from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from tqdm import tqdm
@@ -14,6 +13,26 @@ IMAGE_TYPE_GRAYSCALE = "/plantvillage dataset/grayscale/"
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SPLIT_OUT_DIR = os.path.join(PROJECT_ROOT, "data")
+
+DATA_TRANSFORMS = {
+    "train": transforms.Compose(
+        [
+            # Add transforms
+            transforms.ToTensor(),
+            transforms.Normalize(  # Adjust for pre trained model
+                [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+            ),
+        ]
+    ),
+    "val": transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(  # Adjust for pre trained model
+                [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
+            ),
+        ]
+    ),
+}
 
 CLASSES = [
     "Apple___Apple_scab",
@@ -52,17 +71,12 @@ def split_data():
             files.append(os.path.join(label_dir, img))
             labels.append(label)
 
-    train_paths, temp_paths, train_labels, temp_labels = train_test_split(
-        files, labels, stratify=labels, test_size=0.3, random_state=27
-    )
-
-    test_paths, val_paths, test_labels, val_labels = train_test_split(
-        temp_paths, temp_labels, stratify=temp_labels, test_size=1 / 3, random_state=27
+    train_paths, test_paths, train_labels, test_labels = train_test_split(
+        files, labels, stratify=labels, test_size=0.2, random_state=42
     )
 
     copy_files(train_paths, train_labels, "train")
     copy_files(test_paths, test_labels, "test")
-    copy_files(val_paths, val_labels, "val")
 
 
 def copy_files(paths, labels, split):
@@ -73,37 +87,13 @@ def copy_files(paths, labels, split):
         shutil.copy(path, dest_dir)
 
 
-def get_dataloaders(batch_size):
-    train_dataset = ImageFolder(
-        root=os.path.join(SPLIT_OUT_DIR, "train"),
-        transform=transforms.Compose(
-            [
-                # Add transforms
-                transforms.ToTensor(),
-                transforms.Normalize(  # Adjust for pre trained model
-                    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-                ),
-            ]
-        ),
-    )
+def get_train_dataset(transform=None):
+    train_path = os.path.join(SPLIT_OUT_DIR, "train")
 
-    val_dataset = ImageFolder(
-        root=os.path.join(SPLIT_OUT_DIR, "val"),
-        transform=transforms.Compose(
-            [
-                # Not sure if we need transforms here
-                transforms.ToTensor(),
-                transforms.Normalize(  # Adjust for pre trained model
-                    [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-                ),
-            ]
-        ),
-    )
+    if not os.path.isdir(train_path) or len(os.listdir(train_path)) == 0:
+        split_data()
 
-    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)  # Shuffle idk???
-    val_loader = DataLoader(val_dataset, batch_size, shuffle=True)  # Shuffle idk???
-
-    return train_loader, val_loader
+    return ImageFolder(root=train_path, transform=transform)
 
 
 if __name__ == "__main__":

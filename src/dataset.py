9b1a0886add596fb5f9bcd31e402cf7bb714204e
jaybrandon <jaybrandon@outlook.com>
2025-11-08 19:12:38 +0100

    feat: add data transforms

--- src/dataset.py
+++ src/dataset.py
@@ -17,13 +17,18 @@ SPLIT_OUT_DIR = os.path.join(PROJECT_ROOT, "data")
 DATA_TRANSFORMS = {
     "train": transforms.Compose(
         [
-            # Add transforms
+            transforms.RandomResizedCrop(256),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]
     ),
     "val": transforms.Compose(
         [
+            transforms.Resize(256),
+            transforms.CenterCrop(256),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]

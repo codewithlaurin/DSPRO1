8a6d61f93aadedf3ffdc2d226426d7a3ca3ba98e
jaybrandon <jaybrandon@outlook.com>
2025-11-12 15:38:09 +0100

    feat: adjust crop size

--- src/dataset.py
+++ src/dataset.py
@@ -20,7 +20,7 @@ BINARY_OUT_DIR = os.path.join(PROJECT_ROOT, "data_binary")
 DATA_TRANSFORMS = {
     "train": transforms.Compose(
         [
-            transforms.RandomResizedCrop(256),
+            transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(15),
             transforms.ColorJitter(
@@ -33,7 +33,7 @@ DATA_TRANSFORMS = {
     "val": transforms.Compose(
         [
             transforms.Resize(256),
-            transforms.CenterCrop(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ]
